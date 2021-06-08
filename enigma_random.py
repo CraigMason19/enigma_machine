38f5755a3cf19309d806bc4acab512e6c5a862c9
SadHappy <craigjm19@gmail.com>
2021-06-08 17:59:58 +0100

    Documented Enigma Random

--- enigma_random.py
+++ enigma_random.py
@@ -1,3 +1,13 @@
+#-------------------------------------------------------------------------------
+# Name:        enigma_random.py
+#
+# Notes:       A helpful set of functions to test random enigma configurations. 
+#
+# Links:
+#
+# TODO:
+#-------------------------------------------------------------------------------
+
 import sys
 sys.path.append('../../Helper')
 
@@ -6,18 +16,60 @@ import letters
 from enigma_machine import M3EnigmaMachine
 
 def random_reflector():
+    """Returns a random reflector string
+
+        Args:
+            None.
+
+        Returns:
+            A string containing a random Reflector ID.
+    """ 
     return random.choice(["UKW-C", "UKW-B"])
 
 def random_rotors():
+    """Returns a random list of 3 rotor id's (Roman numerals) from a set of 5 
+
+        Args:
+            None.
+
+        Returns:
+            A list of 3 strings in Roman numeral form.
+    """ 
     return random.sample(set(["I", "II", "III", "IV", "V"]), 3)
 
 def random_start_positions():
+    """Returns a random list of 3 letters representing where the wheels are 
+       rotated too.
+
+        Args:
+            None.
+
+        Returns:
+            A list of 3 letters.
+    """ 
     return [letters.random_upper_letter() for i in range(3)]
 
 def random_ring_settings():
+    """Returns a random list of 3 letters representing the ring settings 
+
+        Args:
+            None.
+
+        Returns:
+            A list of 3 letters.
+    """ 
     return [letters.random_upper_letter() for i in range(3)]
 
 def random_plugboard():
+    """Returns a random string of 10 plug combinations in the format
+       'XX XX XX XX...'.
+
+        Args:
+            None.
+
+        Returns:
+            A string representing the plug combinations.
+    """ 
     alpha_list = list(letters.ALPHABET_UPPER)
     random.shuffle(alpha_list)
 
@@ -25,6 +77,15 @@ def random_plugboard():
     return ' '.join(a+b for a,b in zip(i, i))
 
 def create_random_machine():
+    """Returns a completely random Enigma machine setup, including reflector,
+       rotors, positions, ring settings and plug combinations.
+
+        Args:
+            None.
+
+        Returns:
+            A M3EnigmaMachine class.
+    """ 
     reflector = random_reflector()
     rotors = random_rotors()
     positions = random_start_positions()
@@ -37,7 +98,15 @@ def create_random_machine():
 
     return em
 
-def test_random_settings():
+def print_random_settings():
+    """Prints completely random Enigma components.
+
+        Args:
+            None.
+
+        Returns:
+            None.
+    """ 
     print(f'Reflector: {random_reflector()}')
     print(f'Rotors: {random_rotors()}')
     print(f'Start positions: {random_start_positions()}')
@@ -45,4 +114,4 @@ def test_random_settings():
     print(f'Random plugboard: {random_plugboard()}')    
 
 if __name__ == "__main__":
-    test_random_settings()
+    print_random_settings()
