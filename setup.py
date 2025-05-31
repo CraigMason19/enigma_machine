e0af954c5899b7298670c2def6c0223b3887b8b2
SadHappy <craigjm19@gmail.com>
2025-05-31 11:41:21 +0100

    Added setup.py to allow pip installation

---
+++ setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="M3_enigma_machine",
+    version='1.0.0',
+    packages=find_packages(),
+    author="Craig Mason",
+    description="A package for creating and using a historically accurate WWII M3 Enigma Machine.",
+    url="https://github.com/CraigMason19/enigma_machine",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "Operating System :: OS Independent",
+    ],
+)
