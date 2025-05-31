de5b53aa329df75546b67cfab2adf52ecc90366f
SadHappy <craigjm19@gmail.com>
2025-05-31 17:26:40 +0100

    Updated patch version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="M3_enigma_machine",
-    version='1.0.0',
+    version='1.0.1',
     packages=find_packages(),
     author="Craig Mason",
     description="A package for creating and using a historically accurate WWII M3 Enigma Machine.",
